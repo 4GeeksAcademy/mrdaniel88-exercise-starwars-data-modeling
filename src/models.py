f5175bd5d6422a962791200beae17afc0be9169e
Daniel Martinez <d.martinez74@gmail.com>
2023-05-11 02:45:40 +0000

    relationships created fot planets, people and films

--- src/models.py
+++ src/models.py
@@ -7,26 +7,79 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class People(Base):
+    __tablename__ = 'people'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    # films = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    homeworld = Column(Integer, ForeignKey("planets.id"))
+    mass = Column(Integer, nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    # species = Column(String(250), nullable=False)
+    # starships = Column(String(250), nullable=False)
+    # vehicles = Column(String(250), nullable=False)
+    planet = relationship("Planets")
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    # name = Column(String(250), nullable=False)
+    # birth_year = Column(String(250), nullable=False)
+    # hair_color = Column(String(250), nullable=False)
+    # films = Column(String(250), nullable=False)
+    # eye_color = Column(String(250), nullable=False)
+    # gender = Column(String(250), nullable=False)
+    # height = Column(Integer, nullable=False)
+    # homeworld = Column(String(250), nullable=False)
+    # mass = Column(Integer, nullable=False)
+    # skin_color = Column(String(250), nullable=False)
+    # species = Column(String(250), nullable=False)
+    # starships = Column(String(250), nullable=False)
+    # vehicles = Column(String(250), nullable=False
+
+class Films(Base):
+    __tablename__ = 'films'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    # characters = Column(String(250))
+    director = Column(String(250))
+    episode_id = Column(Integer, nullable=False)
+    opening_crawl = Column(String(250))
+    # planets = Column(String(250))
+    producer = Column(String(250))
+    # release_date = Column(String(250))
+    # species = Column(String(250))
+    # starships = Column(String(250))
+    title = Column(String(250))
+    # vehicles = Column(String(250))
+    # person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(People)
 
-    def to_dict(self):
-        return {}
+class Cast(Base):
+    __tablename__ ='cast'
+    id = Column(Integer, primary_key=True)
+    film_id = Column(Integer, ForeignKey("films.id"))
+    film = relationship(Films, backref="cast")
+    person_id=Column(Integer, ForeignKey("people.id"))
+    person = relationship(People)
 
-## Draw from SQLAlchemy base
+
+
+
+def to_dict(self):
+    return {}
+
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
